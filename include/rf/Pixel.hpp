16b5e072eb1ea83329ed1954ba56f8173b16a5f6
Alfonso Ros <alfonsoros88@gmail.com>
2018-06-07 22:52:43 +0200

    Add Offset struct

--- include/rf/Pixel.hpp
+++ include/rf/Pixel.hpp
@@ -5,6 +5,11 @@
 
 namespace rf {
 
+struct Offset {
+  uint32_t x;
+  uint32_t y;
+};
+
 template <class Image>
 class Pixel {
   uint32_t _x;
@@ -15,6 +20,9 @@ class Pixel {
   using value_type = typename Image::value_type;
 
   value_type const& value() const noexcept { return _img(_x, _y); }
+  value_type const& value(Offset const& off) const noexcept { 
+    return _img(_x + off.x, _y + off.y); 
+  }
 
   Pixel(uint32_t x, uint32_t y, Image const& img) noexcept : _x{x},
                                                              _y{y},
